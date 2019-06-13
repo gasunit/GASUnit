7b83b4cb04feecb04c43144a93a65bde39b62d4b
Munieru <20086673+munierujp@users.noreply.github.com>
2019-06-13 17:19:06 +0900

    Convert util.js to util.ts

--- src/util.ts
+++ src/util.ts
@@ -1,10 +1,8 @@
 /**
 * Return whether value is plain object.
-* @param {Object} value
-* @return {Boolean} whether value is plain object
 */
 /* eslint-disable-next-line no-unused-vars */
-function isPlainObject_ (value) {
+function isPlainObject_ (value: any): boolean {
   return value !== null &&
     value !== undefined &&
       Object.prototype.toString.call(value) === '[object Object]'
@@ -12,30 +10,24 @@ function isPlainObject_ (value) {
 
 /**
 * Return whether value is boolean.
-* @param {Object} value
-* @return {Boolean} whether value is boolean
 */
-function isBoolean_ (value) {
+function isBoolean_ (value: any): boolean {
   return typeof value === 'boolean'
 }
 
 /**
 * Return whether value is function.
-* @param {Object} value
-* @return {Boolean} whether value is function
 */
 /* eslint-disable-next-line no-unused-vars */
-function isFunction_ (value) {
+function isFunction_ (value: any): boolean {
   return typeof value === 'function'
 }
 
 /**
 * Create indent string.
-* @param {Number} level - indent level
-* @return {String} indent string
 */
 /* eslint-disable-next-line no-unused-vars */
-function indent_ (level) {
+function indent_ (level: number): string {
   var indent = ''
   for (var i = 0; i < level; i++) {
     indent += '  '
@@ -45,11 +37,9 @@ function indent_ (level) {
 
 /**
 * Return whether all test is passing.
-* @param {Object} result
-* @return {Boolean} whether all test is passing
 */
 /* eslint-disable-next-line no-unused-vars */
-function isAllPassing_ (result) {
+function isAllPassing_ (result: object): boolean {
   for (var key in result) {
     var value = result[key]
     var passing = value.passing
