7fc29e84a19ab1351856dca73e1efe3e34208e1d
Munieru <20086673+munierujp@users.noreply.github.com>
2019-06-27 16:56:38 +0900

    Add exports.ts

---
+++ src/exports.ts
@@ -0,0 +1,28 @@
+import exportsEach_ from './exportsEach_'
+import format_ from './format_'
+
+// NOTE: Below function will be public in Google Apps Script.
+/**
+* Execute test bys Exports style.
+*
+*   exports({
+*     'Array': {
+*       '#indexOf()': {
+*         'should return -1 when not present': function () {
+*           assert([1, 2, 3].indexOf(4) === -1)
+*         },
+*         'should return the index when present': function () {
+*           assert([1, 2, 3].indexOf(3) === 3)
+*         }
+*       }
+*     }
+*   })
+*/
+// @ts-ignore: Duplicate identifier 'exports'.
+function exports (suite: object) {
+  var result = exportsEach_(suite)
+  Logger.log(format_(result))
+}
+
+// NOTE: Below statement will be removed by clasp.
+export default exports
